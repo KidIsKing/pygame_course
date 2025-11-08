ec273b8db1751c039d093fcef06a91f2021184bb
KidIsKing <anulamasalova@gmail.com>
2025-11-08 15:45:54 +0500

    finish 2nd lesson

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pygame
 
 
-# Constans
+# Константы
 SCREEN_HEIGHT = 600
 SCREEN_WIDHT = 300
 
 BACKGROUND_COLOR = (92, 215, 246)
+
+SQUARE_HEIGHT = 100
+SQUARE_WIDTH = 80
+SQUARE_COLOR = (255, 255, 255)
+
+CIRCLE_COLOR = "Red"
+CIRCLE_RADIUS = 22
+
+MAIN_FONT_SIZE = 30
+
+JAPAN_TEXT_COLOR = (188, 126, 230)
 
 
 pygame.init()
@@ -19,6 +30,14 @@ screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDHT))
 # Подгрузка и установка иконки игры
 icon = pygame.image.load("images/icon.png")
 pygame.display.set_icon(icon)
+
+# Создание квадрата, как объекта игры, в несколько строк
+square = pygame.Surface((SQUARE_HEIGHT, SQUARE_WIDTH))
+square.fill(SQUARE_COLOR)
+
+# Создание текста
+main_font = pygame.font.Font("fonts/Bold.ttf", MAIN_FONT_SIZE)
+japan_text = main_font.render("Japan", False, JAPAN_TEXT_COLOR)
 
 
 def key_control():
@@ -36,6 +55,17 @@ def main():
     screen.fill(BACKGROUND_COLOR)
 
     while True:
+        screen.blit(square, (SCREEN_HEIGHT // 2, SQUARE_WIDTH // 2))
+        screen.blit(japan_text, (SQUARE_HEIGHT + 200, SQUARE_WIDTH - 80))
+
+        # Рисуем круг одной* строкой в квадрате
+        pygame.draw.circle(
+            square,
+            CIRCLE_COLOR,
+            (SQUARE_HEIGHT // 2, SQUARE_WIDTH // 2),
+            CIRCLE_RADIUS
+        )
+
         key_control()
 
         pygame.display.update()
